3c86983fc60a1792cab2558e408ec1523a0622a0
hilel01 <sofiaromanyk374@gmail.com>
2026-08-25 15:04:30 +0300

    список розвертає останню цифру

--- 3.2.py
+++ 3.2.py
@@ -1,21 +1,5 @@
-numbers = [1,2,3,4]         #перший випадок
+numbers = [1,2,3,4,5,6,]
 if len(numbers) >1:
     numbers = numbers[-1:] + numbers[:-1]
 print(numbers)
 
-#другий випадок
-numbers = [1]
-if len(numbers) >1:
-    numbers = numbers[-1:] + numbers[:-1]
-print(numbers)
-
-#третій випадок
-numbers = []
-if len(numbers) >1:
-    numbers = numbers[-1:] + numbers[:-1]
-print(numbers)
-
-#четвертий випадок
-numbers = [1,2,3,4,5]
-if len(numbers) >1:
-    print(numbers)
