ea9765f4df9670f98bb5a47dd8699bd5c1216b51
hilel01 <sofiaromanyk374@gmail.com>
2026-09-08 18:05:55 +0300

    модифікувати рядок

---
+++ 7.2.py
@@ -0,0 +1,9 @@
+def correct_sentence(text):
+    text = text[0].upper() + text[1:]
+
+    if not text.endswith("."):
+        text += "."
+
+    return text
+print(correct_sentence("hi."))
+print(correct_sentence("hello"))
